905af40d5e38bef908327649d61f7914c1d21cfa
tjtreem <tjtreem@gmail.com>
2026-09-09 04:41:47 -0400

    Add initial Go entry point

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tjtreem/tinyLLM
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func greet() string {
+	return "tinyLLM booting..."
+
+}
+
+func main() {
+	fmt.Println(greet())
+
+}
